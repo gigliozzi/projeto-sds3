e59be56a715374dc15bb88e061754b38a56983ff
William Gigliozzi <gigliozzi@gmail.com>
2021-05-05 14:11:58 -0300

    Mockup table

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -16,93 +16,93 @@ const DataTable = () => {
                     <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
+                        <td>22</td>
+                        <td>18</td>
+                        <td>12015.00</td>
+                    </tr>
+                    <tr>
+                        <td>18/04/2021</td>
+                        <td>Ana Maria</td>
+                        <td>15</td>
+                        <td>10</td>
+                        <td>11010.00</td>
+                    </tr>
+                    <tr>
+                        <td>15/04/2021</td>
+                        <td>Mário César</td>
                         <td>34</td>
+                        <td>25</td>
+                        <td>16200.00</td>
+                    </tr>
+                    <tr>
+                        <td>16/04/2021</td>
+                        <td>Denis Quaid</td>
+                        <td>25</td>
+                        <td>6</td>
+                        <td>10012.00</td>
+                    </tr>
+                    <tr>
+                        <td>19/04/2021</td>
+                        <td>Nancy Alves</td>
+                        <td>18</td>
+                        <td>11</td>
+                        <td>13017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Marlene Santos</td>
+                        <td>29</td>
+                        <td>19</td>
+                        <td>14017.00</td>
+                    </tr>
+                    <tr>
+                        <td>21/04/2021</td>
+                        <td>Paulo Augusto</td>
+                        <td>30</td>
+                        <td>10</td>
+                        <td>17011.00</td>
+                    </tr>
+                    <tr>
+                        <td>27/04/2021</td>
+                        <td>Sara Ribeiro</td>
+                        <td>31</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
+                        <td>23/04/2021</td>
+                        <td>Roberto Souza</td>
+                        <td>21</td>
+                        <td>15</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
+                        <td>16/04/2021</td>
+                        <td>Maria Barros</td>
                         <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>34</td>
+                        <td>28000.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
+                        <td>Stwart Little</td>
                         <td>34</td>
                         <td>25</td>
-                        <td>15017.00</td>
+                        <td>18015.00</td>
+                    </tr>
+                    <tr>
+                        <td>20/04/2021</td>
+                        <td>Bruce Wayne</td>
+                        <td>32</td>
+                        <td>20</td>
+                        <td>18014.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Vanessa Santos</td>
+                        <td>30</td>
+                        <td>19</td>
+                        <td>14017.00</td>
                     </tr>
                     
                 </tbody>
